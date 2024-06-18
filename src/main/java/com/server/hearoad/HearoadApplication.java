bafe27185da53d1de2016a3973787feccfab0ccb
0gonge <Challengef0802@gmail.com>
2024-06-18 14:25:19 +0900

    Initial Setting

---
+++ src/main/java/com/server/hearoad/HearoadApplication.java
@@ -0,0 +1,13 @@
+package com.server.hearoad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HearoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HearoadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/hearoad/HearoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.hearoad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HearoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
